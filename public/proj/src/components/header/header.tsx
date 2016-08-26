abf7fee20704ea69ae254496c9374b5e7f5ccb31
mortonprod <mortonprod@gmail.com>
2016-08-26 23:40:14 +0100

    Add the beginnings of the react app.

---
+++ public/proj/src/components/header/header.tsx
@@ -0,0 +1,30 @@
+﻿/// <reference path="../../../typings/index.d.ts" />
+import * as React from "react";
+///TODO:Can't use import in index like angular 2 files?????
+import { Bulb } from "../shared/bulb/bulb";
+import { Arrow } from "../shared/arrow/arrow";
+
+import * as ReactDOM from "react-dom";
+
+interface IHeaderProps { link:string };
+
+///TODO: Important: Classname/idName not class/id in jsx.
+export class Header extends React.Component<IHeaderProps, {}> {
+    ///TODO:Pass in properties like:
+    /// <Header prop1="something" prop2="blah" /> when you ReactDom.render
+
+    ///TODO:Remember jsx expression must have one parent element.
+    render() {
+        return (
+            <div className="header">
+                <Bulb/>
+                {this.props.children}
+                <Arrow direction="down" link={this.props.link} />
+            </div>
+        );
+    }
+}
+//ReactDOM.render(
+//    <Bulb compiler="TypeScript" framework="React" />,
+//    document.getElementById("header")
+//);
---
+++ public/proj/src/components/shared/arrow/arrow.tsx
@@ -0,0 +1,40 @@
+﻿/// <reference path="../../../../typings/index.d.ts" />
+import * as React from "react";
+///Properties(Input)
+interface IArrowProps { direction: string; link: string; }
+///State:
+///Hover(Css) >>> Make bigger and wiggle.
+///Html
+///<Arrow direction="down" link="http:....#top"/>
+
+export class Arrow extends React.Component<IArrowProps, {}> {
+    arrowType = "arrow";
+    ///Props will come from parent on rendering.
+    constructor(props: IArrowProps) {
+        ///Super will call constructor of base class react.
+        ///TODO:Still don't quite get this???
+        super(props);
+        switch (this.props.direction) {
+            case "up": 
+                this.arrowType += " glyphicon glyphicon-chevron-up"
+                break;
+            case "down":
+                this.arrowType += " glyphicon glyphicon-chevron-down"
+                break;
+            case "left":
+                this.arrowType += " glyphicon glyphicon-chevron-left"
+                break;
+            case "right":
+                this.arrowType += " glyphicon glyphicon-chevron-right"
+                break;
+            default:
+                throw new TypeError("Direction should be up/down/left/right. Provided: " + props.direction);
+
+        }
+    }
+    render() {
+        return (
+            <a className={this.arrowType} href={this.props.link}>here</a>
+        );
+    }
+}
