4172dc746ed9d9712e2585b8c3decaab896711d9
mortonprod <mortonprod@gmail.com>
2016-09-10 01:32:43 +0100

    Add karma config file.
    ----
    This returns errors with spec files. DO NOT USE UNTIL UPDATED

---
+++ public/proj/karma.conf.js
@@ -0,0 +1,32 @@
+﻿var webpackConfig = require('./webpack.config.js');
+
+module.exports = function (config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+            'src/**/*spec.js',
+            'src/**/*spec.jsx',
+            'dist/vendors.js',
+            'dist/bundle.js'
+
+        ],
+        ///["webpack"] will tell karma to run this through webpack first.
+        preprocessors: {
+            'src/**/*spec.js': ['webpack', 'sourcemap'],
+            'src/**/*spec.jsx': ['webpack', 'sourcemap'],
+            'dist/vendors.js': ['webpack','sourcemap'],
+            'dist/bundle.js': ['webpack','sourcemap']
+
+        },
+        webpack: webpackConfig,
+        reporters: ['mocha'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        singleRun: true,
+        concurrency: Infinity
+    })
+}
