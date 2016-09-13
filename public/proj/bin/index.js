3be9b290bf6746097f0ef8b1ce550776cf97bf3a
mortonprod <mortonprod@gmail.com>
2016-09-13 22:02:33 +0100

    Add the entry point for node

---
+++ public/proj/bin/index.js
@@ -0,0 +1,9 @@
+﻿/////Entry point for the app "node (this file)"  to run
+var debug = require('debug')('blog');
+var app = require('../app');
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    debug('Express server listening on port ' + server.address().port);
+});
