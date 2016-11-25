d6c66a246a82ff47ca98553ae28a126281a2b6a2
mortonprod <mortonprod@gmail.com>
2016-11-25 21:18:20 +0000

    Add test dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 ﻿'use strict';
 //var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
+var isProd = (process.env.NODE_ENV === 'production');
+console.log("production:" + isProd);
+let outDirLocalAndDev =null;
+if (!isProd) {//IMPORTANT:Will only output if absolute for dev server and relative for production builds.
+    outDirLocalAndDev = '/dist/bundle/';
+} else {
+    outDirLocalAndDev = './dist/bundle/';
+}
 var config = {
     entry: {
         index: ['./src/client/index.tsx'],
+        tests: ['./src/client/__tests__/boxAnimation.spec.ts'],
         vendor: ['react','react-dom','redux', 'react-redux','velocity-animate','velocity-react','velocity-animate/velocity.ui']
 
     },
@@ -18,7 +27,7 @@ var config = {
     ],
 
     output: {
-        path: '/dist/bundle',
+        path: outDirLocalAndDev,
         filename: '[name].js',
         libraryTarget: 'umd' // Need this for static site generation.
     },
