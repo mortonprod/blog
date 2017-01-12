c7917efed5e43715a500e38efe36864c7e0ecba5
mortonprod <mortonprod@gmail.com>
2017-01-12 23:49:37 +0000

    Update webpack to work with single page

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,14 @@
 ﻿'use strict';
-//var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
-var isProd = (process.env.NODE_ENV === 'production');
-console.log("production:" + isProd);
-let outDirLocalAndDev =null;
-if (!isProd) {//IMPORTANT:Will only output if absolute for dev server and relative for production builds.
-    outDirLocalAndDev = '/dist/bundle/';
-} else {
-    outDirLocalAndDev = './dist/bundle/';
-}
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var config = {
     entry: {
-        index: ['./src/client/index.tsx'],
-        tests: ['./src/client/__tests__/boxAnimation.spec.ts'],
-        vendor: ['react','react-dom','redux', 'react-redux','velocity-animate','velocity-react','velocity-animate/velocity.ui']
-
+        index: ['./src/client/index.ts'],
     },
     plugins: [
-//        new ExtractTextPlugin("site.css"),
-        new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
-//        new webpack.ProvidePlugin({
-//            jQuery: 'jquery',
-//            $: 'jquery',
-//            jquery: 'jquery'
-//        })
     ],
-
     output: {
-        path: outDirLocalAndDev,
+        path: "./dist/assets/bundle",
         filename: '[name].js',
         libraryTarget: 'umd' // Need this for static site generation.
     },
@@ -37,20 +18,22 @@ var config = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style-loader!css-loader" },
-    //       {
-    //           test: /\.scss$/,
-    //           loader: ExtractTextPlugin.extract(
-    //               //Need:?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5] to set the right name for each css!
-    //               "style",
-    //               "css!postcss-loader!sass")
-    //       },
-            //  { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery' },
-          { test: /\.tsx?$/, loader: "ts-loader" },
-          {
-              test: /\.(png|ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-              loader: 'file-loader',
-          }
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader','css-loader','postcss-loader', 'sass-loader']
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style-loader', 'css-loader']
+            },
+            { test: /\.ts?$/, loader: "ts-loader" },
+            {
+                test: /\.(png|ttf|eot|svg|woff(2)?)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 25000
+                }
+            }
         ]
     }
 };
